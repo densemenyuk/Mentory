7e46ac6c3b5eaa313ca657463307dd06b31becdf
Denis <dinozavr.independent228@gmail.com>
2025-11-27 21:39:22 +0500

    7 Урок, знакомтсво с JS

---
+++ November/lesson_8/script.js
@@ -0,0 +1,42 @@
+console.log('Сегодня восьмой урок');
+
+// alert('Сегодня восьмой урок');
+
+// confirm('Сегодня точно восьмой урок?');
+
+// prompt('Какой сегодня урок?');
+
+// console.log(confirm('Сегодня точно восьмой урок?'));
+// console.log(prompt('Какой сегодня урок?'));
+
+// Не пользуются, но можно встретить в старых бибилотеках, кодах
+// var variable1 = 'Сегодня урок про var' 
+
+// let variable2 = 'Сегодня урок про let'
+
+// const variable3 = 'Сегодня урок про константу'
+
+// console.log(variable1);
+// console.log(variable2);
+// console.log(variable3);
+
+// const num1 = 5
+// const num2 = 10
+
+
+// console.log(2 + 2);
+// console.log(num1 + num2);
+
+// const summ = num1 + num2
+
+// console.log(summ);
+
+// const userMessage = prompt('Ваша сообщение?')
+
+// console.log(userMessage);
+
+
+// CONST могут писаться большими буквами
+// const BLA = 2;
+
+// КЛЮЧ ? - при работе с объектами и выстакиванием данных
