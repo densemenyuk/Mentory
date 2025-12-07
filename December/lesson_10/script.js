c7c669579ad21eb8afc24eb74ecf70212814cbeb
Denis <dinozavr.independent228@gmail.com>
2025-12-07 13:28:42 +0500

    Добавил материалы 10-го урока

--- December/lesson_10/script.js
+++ December/lesson_10/script.js
@@ -1,25 +1,82 @@
-// const modalOpenBtn = document.querySelector('.open-modal')
-// const modal = document.querySelector('.modal')
 
-// modalOpenBtn.addEventListener('click', ()=>{
-//     modal.classList.add('open')
-// })
-
-
+// Модалки простые
+// const modalOpenBtns = document.querySelectorAll('.open-modal');
 // const modalCloseBtn = document.querySelector('.close-modal')
 
-// modalCloseBtn.addEventListener('click', ()=>{
-//     modal.classList.remove('open')
+// const modals = document.querySelectorAll('.modal');
+
+
+// modalCloseBtn.addEventListener('click', () => {
+//   modal.classList.remove('open');
+// });
+
+// modalOpenBtns.forEach((item) => {
+//   item.addEventListener('click', () => {
+//     modals[0].classList.add('open');
+//   });
 // })
 
 
-const openModalButtons = document.querySelector('[data-modal-action="modal"]')
-const closeOpenButtons = document.querySelector('data-modal-action="close-modal"')
+// Модалки продвинутые
+const openModalButtons = document.querySelectorAll('[data-modal-action="open-modal"]');
+const closeModalButtons = document.querySelectorAll('[data-modal-action="close-modal"]');
+const modalWindows = document.querySelectorAll('[data-modal-action="modal"]');
 
-const ModalWindows = document.querySelector('data-modal-action="modal"')
 
-openModalButtons.forEach((btn)=> {
-    btn.addEventListener('click', (event) =>
-    console.log(event.target)
-    );
+openModalButtons.forEach((btn) => {
+  btn.addEventListener('click', openModal);
+});
+
+closeModalButtons.forEach((btn) => {
+  btn.addEventListener('click', closeModal);
+});
+
+
+function openModal(event) {
+  modalWindows.forEach((modal) => {
+    if (modal.getAttribute('data-modal') === event.target.getAttribute('data-modal')) {
+      modal.classList.add('open')
+    }
+  });
+}
+
+function closeModal(event) {
+  modalWindows.forEach((modal) => {
+    if (modal.getAttribute('data-modal') === event.target.getAttribute('data-modal')) {
+      modal.classList.remove('open')
+    }
+  });
+}
+
+// Аккордионы
+
+const accordions = document.querySelectorAll('.accordion__container');
+
+function toggleAccordion(event) {
+  const accordionContainer = event.target.closest('[accordion-type]') ?? event.target.closest('.accordion__container');
+  const accordionType = event.target.closest('[accordion-type]')?.getAttribute('accordion-type') ?? 'multi';
+  const isAccordionHeader = event.target.classList.contains('accordion__header');
+
+
+  if (isAccordionHeader) {
+    const parentElement = event.target.closest('[accordion-id]');
+    const accordionState = parentElement.getAttribute('accordion-state');
+
+    if (accordionType === 'single') {
+      const accordions = accordionContainer.querySelectorAll('[accordion-id]');
+      accordions.forEach((item) => {
+        item.setAttribute('accordion-state', 'closed')
+      })
+    }
+
+    if (accordionState === 'closed') {
+      parentElement.setAttribute('accordion-state', 'open')
+    } else {
+      parentElement.setAttribute('accordion-state', 'closed')
+    }
+  }
+}
+
+accordions.forEach((accordion) => {
+  accordion.addEventListener('click', toggleAccordion)
 })
