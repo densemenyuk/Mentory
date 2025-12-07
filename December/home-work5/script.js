2bb825dcab9b32b4107edaedae9913b6e6d9422b
Denis <dinozavr.independent228@gmail.com>
2025-12-07 19:37:03 +0500

    Выполнил еще одно задание

--- December/home-work5/script.js
+++ December/home-work5/script.js
@@ -1,11 +1,11 @@
 // 1. Обмен значениями двух переменных
-// Создать две переменные a и b и поменять их значения местами без использования третьей переменной. 
+// Создать две переменные a и b и поменять их значения местами без использования третьей переменной.
 
 let a = 5;
 let b = 10;
 console.log(`a = ${a}, b = ${b}`);
 
-[a, b] = [b, a]
+[a, b] = [b, a];
 console.log(`a = ${a}, b = ${b}`);
 
 // 2. Проверка чётного или нечётного числа
@@ -15,18 +15,17 @@ console.log(`a = ${a}, b = ${b}`);
 
 // false — если нечётное
 
-arr = [2,4,2345,127,437,47,45,86,20]
+arr = [2, 4, 2345, 127, 437, 47, 45, 86, 20];
 
-function number () {
-    arr.forEach((element) => {
-        if (element % 2 == 0) {
-            console.log(`${element} это четное число`);
-            
-        } else {
-            console.log(`${element} это нечетное число`);
-        };
-    });
-};
+function number() {
+  arr.forEach((element) => {
+    if (element % 2 == 0) {
+      console.log(`${element} это четное число`);
+    } else {
+      console.log(`${element} это нечетное число`);
+    }
+  });
+}
 number();
 
 // 3. Сумма чисел от 1 до N
@@ -35,13 +34,12 @@ number();
 // Пример:
 // sumTo(5) → 15
 
-
-function sumNumber (number) {
-    let sum = 0
-    for (let i = 0; i < number; i++) {
-        sum +=i
-    }
-    console.log(`Сумма ${sum}`);
+function sumNumber(number) {
+  let sum = 0;
+  for (let i = 0; i < number; i++) {
+    sum += i;
+  }
+  console.log(`Сумма ${sum}`);
 }
 
 sumNumber(10);
@@ -52,15 +50,30 @@ sumNumber(10);
 // Пример:
 // "hello" → "olleh"
 
-let text = 'hello world'
+let text = "hello world";
 
-function reverseText () {
-    let reversed = ''
-    for (i = text.length - 1; i >=0 ; i --) {
-        reversed += text[i]
-    }
-    console.log(reversed);
+function reverseText() {
+  let reversed = "";
+  for (i = text.length - 1; i >= 0; i--) {
+    reversed += text[i];
+  }
+  console.log(reversed);
 }
 
 reverseText();
- 
+
+// 5. Фильтрация массива по чётным числам
+// Написать функцию filterEven(arr), которая возвращает новый массив только с чётными значениями.
+
+arr2 = [23,6,13, 44, 23456, 1272, 4347, 47, 45, 86, 20, 46, 28, 21, 44, 22, 90, 86];
+
+function newArray() {
+  const array = [];
+  arr2.forEach((element) => {
+    if (element % 2 == 0) {
+      array.push(element);
+    }
+  });
+  console.log(array);
+}
+newArray();
