26d02aaf5bad71139eed6b50027998a79a60e76c
Denis <dinozavr.independent228@gmail.com>
2025-12-16 18:02:51 +0500

    Добавил домашку галерия masonry

---
+++ December/home-work/home-work7/script.js
@@ -0,0 +1,12 @@
+const lightbox = GLightbox({
+  touchNavigation: true,
+  loop: true,
+  autoplayVideos: true,
+  descPosition: "right",
+  type: "image",
+  effect: "fade",
+  width: "900px",
+  height: "auto",
+  zoomable: true,
+  draggable: true,
+});
