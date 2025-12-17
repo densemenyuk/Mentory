dbfb12458b37259aace25d33cd97ddf314a68bc5
Denis Semenyuk <semenyuk@MacBook-Air-Denis.local>
2025-12-17 15:03:01 +0500

    Внес правки и медиа запросы

--- December/home-work/home-work6/script.js
+++ December/home-work/home-work6/script.js
@@ -11,16 +11,32 @@ let swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
   breakpoints: {
+    344: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    359: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    375: {
+      slidesPerView: 1,
+      spaceBetween: 5,
+    },
+    639: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
     640: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
     768: {
-      slidesPerView: 4,
-      spaceBetween: 40,
+      slidesPerView: 2,
+      spaceBetween: 30,
     },
     1024: {
-      slidesPerView: 4,
+      slidesPerView: 3,
       spaceBetween: 50,
     },
   },
